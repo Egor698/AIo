f50bb607d0e12ac8e9164830e489f0005c40cec4
EgorkaFranklin <150540882+Egor698@users.noreply.github.com>
2026-02-23 14:12:24 +0300

    Delete AiogramProjectsLesson directory

--- AiogramProjectsLesson/app/handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Dispatcher
-from app.handlers.main import router as router_main
-from app.handlers.catalog import router as router_info
-from app.handlers.cart import router as router_cart
-
-def include_routers(dp: Dispatcher):
-    dp.include_routers(router_main, router_info, router_cart)
